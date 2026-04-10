ba89df340cf5255d67335bb0c56c260d99705d02
Haadi-Shaikh <155647248+Haadi-Shaikh@users.noreply.github.com>
2026-04-10 18:54:53 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+if os.environ.get('CREATE_SUPERUSER') == 'True':
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@example.com',
+            password='admin123'
+        )
