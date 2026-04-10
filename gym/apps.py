74283d6c68adb1ef684fa118fe18546cdbdc7c7a
Haadi-Shaikh <155647248+Haadi-Shaikh@users.noreply.github.com>
2026-04-10 19:01:12 +0530

    Update apps.py

--- gym/apps.py
+++ gym/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
 
-
 class GymConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gym'
+
+    def ready(self):
+        import os
+        if os.environ.get('CREATE_SUPERUSER') == 'True':
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser(
+                    username='admin',
+                    email='admin@example.com',
+                    password='admin123'
+                )
